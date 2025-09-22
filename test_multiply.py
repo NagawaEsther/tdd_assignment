661a82872af146ab644838bbf5f1ce4af289ebab
NagawaEsther <nagawaesther227@gmail.com>
2025-09-22 11:50:00 +0300

    assignment

---
+++ test_multiply.py
@@ -0,0 +1,16 @@
+from multiply import multiply
+
+def test_multiply_1_1():
+    assert multiply(1, 1) == 1
+
+def test_multiply_2_2():
+    assert multiply(2, 2) == 4
+
+def test_multiply_3_3():
+    assert multiply(3, 3) == 9
+
+def test_multiply_4_4():
+    assert multiply(4, 4) == 16
+
+def test_multiply_23_45():
+    assert multiply(23, 45) == 1035
